1d7fc8d133ddde74c8f7a3837668fe5c6f6a24c8
NilsRublein <30392686+NilsRublein@users.noreply.github.com>
2020-08-25 16:52:04 +0200

    Increased range of possible pitches to C5 / E3

--- PitchDetection.m
+++ PitchDetection.m
@@ -4,15 +4,12 @@ function NoteData = PitchDetection(NoteLoc, BarLoc)
 %saved in a new data structure that also contains the note locations and
 %the system
 
-%% TO DO:
+%% Keep in mind:
 %{
 1)Assumes perfect y location. Still need to account for what happens if
 note.y is slightly higher located than ref_line
 
-2)Currently only works up to F4 / A2. Should include higher notes too (C5/F3).
-need to adjust ref_line for that (e.g. multiply ref_line with staff_space/2 or sth, yada yada)
-
-3) Does not consider flats or sharps!
+2) Does not consider flats or sharps!
 %}
 
 
@@ -27,15 +24,13 @@ key = num2cell(zeros(length(NoteData),1));
 staff_space =  frequency(BarLoc(:,2));
 
 %% Define Pitches
-%For Convenience I start at F4, but you can also include higher notes if
-%you adjust the reference line
-G_pitches = {'F4','E4','D4','C4','B3','A3','G3','F3','E3','D3','C3','B2','A2','G2','F2','E2','D2','C2','B1','A1','G1'};
-bass_pitches = {'A2','G2','F2','E2','D2','C2','B1','A1','G1','F1','E1','D1','C1','B0','A0','G0','F0','E0','D0','C0','B00'};
+G_pitches = {'C5','B4','A4','G4','F4','E4','D4','C4','B3','A3','G3','F3','E3','D3','C3','B2','A2','G2','F2','E2','D2','C2','B1','A1','G1'};
+bass_pitches = {'E3','D3','C3','B2','A2','G2','F2','E2','D2','C2','B1','A1','G1','F1','E1','D1','C1','B0','A0','G0','F0','E0','D0','C0'};
 
 
 %% Find reference lines, check whether we have both G and bass keys or not
 %Get the first lines
-ref_line = find(BarLoc(:,3)==1);
+ref_line = find(BarLoc(:,3)==1); %The top line of every staff block, so either F4 (G-Key) or A2 (Bass Key)
 
 %Check if we have both G and bass keys or not
 %If std of BarLoc(ref_line(:),2) is bigger than e.g. staff_space, we
@@ -67,7 +62,9 @@ for i=1:length(NoteLoc)
         %the G key
         if abs(NoteLoc(i).Centroid_2 - midBars((sys*2)-1)) < abs(NoteLoc(i).Centroid_2 - midBars(sys*2))
             
-            pitches{i}=(NoteLoc(i).Centroid_2 - BarLoc(ref_line((sys*2)-1)))/diff; %multiply sys by 2 and go one back to go to the ref_line of the G key
+            % multiply sys by 2 and go one back to go to the ref_line of the G key which are all odd ref lines.
+            % -4*diff moves the ref_line from F4 to C5. 
+            pitches{i}=(NoteLoc(i).Centroid_2 - BarLoc(ref_line((sys*2)-1))+4*diff)/diff;  
             if (pitches{i} <= length(G_pitches)) && (pitches{i} >= 0) %avoid out of bounds error
                 pitches{i}= G_pitches{round(pitches{i})+1}; %add pitch into our data structure
                 key{i} = 'G-Key';
@@ -76,7 +73,7 @@ for i=1:length(NoteLoc)
             end
             
         else % we must be in the bass key
-            pitches{i}=(NoteLoc(i).Centroid_2 - BarLoc(ref_line(sys*2)))/diff; %multiply sys by 2 to go to the ref_line of the bass key
+            pitches{i}=(NoteLoc(i).Centroid_2 - BarLoc(ref_line(sys*2))+4*diff)/diff; %multiply sys by 2 to go to the ref_line of the bass key which are all even ref_lines
             if pitches{i} <= length(bass_pitches) && (pitches{i} >= 0)
                 pitches{i}= bass_pitches{round(pitches{i})+1};
                 key{i} = 'F-Key';
